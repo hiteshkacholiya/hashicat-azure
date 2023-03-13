65882f2a4463b76e3d8aab24c25fea833a9359c4
Hitesh Kacholiya <hitesh.kacholiya@avanade.com>
2023-03-13 15:00:46 -0500

    ADded network module

--- network.tf
+++ network.tf
@@ -1,6 +1,7 @@
 
 module "network" {
-  source              = "Azure/network/azurerm"
+  source  = "app.terraform.io/hk-chip-org/network/azurerm"
+  version = "3.5.0"
   resource_group_name = var.resource_group_name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
@@ -16,4 +17,9 @@ module "network" {
     environment = "dev"
     costcenter  = "it"
   }
+}
+
+module "network" {
+  
+  # insert required variables here
 }
